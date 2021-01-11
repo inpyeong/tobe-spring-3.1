a4e9272d0f9e4946cc179c0fadf5708f0a983932
inpyeong <jinipyung@gmail.com>
2021-01-11 18:25:34 +0900

    DAO의 확장

--- src/user/dao/DConnectionMaker.java
+++ src/user/dao/DConnectionMaker.java
@@ -4,9 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DUserDao extends UserDao {
-    @Override
-    protected Connection getConnection() throws ClassNotFoundException, SQLException {
+public class DConnectionMaker implements ConnectionMaker {
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?characterEncoding=UTF-8&serverTimezone=UTC", "root", "0824");
         return c;
