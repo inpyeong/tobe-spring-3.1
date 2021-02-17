48c148a3221104b871354117709ddda5a4f9c3e0
inpyeong <jinipyung@gmail.com>
2021-02-17 11:15:45 +0900

    1.7 의존관계 주입(DI)

--- src/user/dao/UserDaoTest.java
+++ src/user/dao/UserDaoTest.java
@@ -1,12 +1,18 @@
 package user.dao;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import user.domain.User;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        // UserDao dao = new DaoFactory().userDao();
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("whiteship");
