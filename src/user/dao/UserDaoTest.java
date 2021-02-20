8eaec735b540321339fcbfef3b1adee6562eafff
inpyeong <jinipyung@gmail.com>
2021-02-21 02:04:33 +0900

    2.1 UserDaoTest 다시 보기, 2.2 UserDaoTest 개선

--- src/user/dao/UserDaoTest.java
+++ src/user/dao/UserDaoTest.java
@@ -1,5 +1,7 @@
 package user.dao;
 
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -7,8 +9,17 @@ import user.domain.User;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+    public static void main(String[] args) {
+        JUnitCore.main("user.dao.UserDaoTest");
+    }
+
+    @Test
+    public void addAndGet() throws ClassNotFoundException, SQLException {
 
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
@@ -22,13 +33,23 @@ public class UserDaoTest {
 
         dao.add(user);
 
-        System.out.println(user.getId() + "등록 성공");
+        System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + " 조회 성공");
+//        System.out.println(user2.getName());
+//        System.out.println(user2.getPassword());
+//
+//        System.out.println(user2.getId() + " 조회 성공");
+//        if (!user.getName().equals(user2.getName())) {
+//            System.out.println("테스트 실패 (name)");
+//        }
+//        else if (!user.getPassword().equals(user2.getPassword())) {
+//            System.out.println("테스트 실패 (password)");
+//        }
+//        else {
+//            System.out.println("조회 테스트 성공");
+//        }
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
     }
-
 }
